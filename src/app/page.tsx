169214646c81d7987e199371d80e29340bc256bd
lntechfuse <techtitannattawat@gmail.com>
2024-09-11 18:15:44 +0700

    error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ import {
   Title,
 } from "@mantine/core";
 
-import { Form, useForm, zodResolver } from "@mantine/form";
+import { useForm, zodResolver } from "@mantine/form";
 import { z } from "zod";
 
 const schema = z
@@ -35,21 +35,27 @@ const schema = z
       .min(3, { message: "Last name must have at least 3 characters" }),
     email: z.string().email({ message: "Invalid email format" }),
     plan: z.enum(["funrun", "mini", "half", "full"], {
-      errorMap: (issue, ctx) => ({ message: "Please select a plan" }),
+      errorMap: () => ({ message: "Please select a plan" }),
     }),
     gender: z.enum(["male", "female"], {
-      errorMap: (issue, ctx) => ({ message: "Please choose a gender" }),
+      errorMap: () => ({ message: "Please choose a gender" }),
     }),
     acceptTermsAndConds: z.literal(true, {
       // message: "You must accept terms and conditions",
-      errorMap: (issue, ctx) => ({
+      errorMap: () => ({
         message: "You must accept terms and conditions",
       }),
     }),
     hasCoupon: z.boolean(),
     coupon: z.string(),
-    password: z.string(),
-    confirmPassword: z.string(),
+    password: z
+      .string()
+      .min(6, { message: "Password must contain at least 6 characters" })
+      .max(12, { message: "Password must not exceed 12 characters" }),
+    confirmPassword: z
+      .string()
+      .min(6, { message:"Password must contain at least 6 characters" })
+      .max(12, {message: "Password must not exceed 12 characters" }),
   })
   .refine(
     //refine allows you check error in your own way
@@ -69,6 +75,12 @@ const schema = z
       message: "Invalid coupon code",
       path: ["coupon"],
     }
+  )
+  .refine((data) => data.password === data.confirmPassword,{
+    message: "Password does not match",
+    path: ["confirmPassword"],
+  }
+
   );
 
 export default function Home() {
@@ -96,10 +108,16 @@ export default function Home() {
     //TIP : get value of currently filled form with variable "form.values"
 
     if (form.values.plan === "funrun") price = 500;
+    if (form.values.plan === "mini") price = 800;
+    if (form.values.plan === "half") price = 1200;
+    if (form.values.plan === "full") price = 1500;
+
     //check the rest plans by yourself
     //TIP : check /src/app/libs/runningPlans.js
 
     //check discount here
+
+    if(form.values.hasCoupon == true && form.values.coupon === "CMU2023") price *=0.7;
 
     return price;
   };
@@ -114,8 +132,8 @@ export default function Home() {
         <Space h="lg" />
 
         {/* add form */}
-        <form onSubmit={form.onSubmit((v) => alert("See you at CMU Marathon"))}>
-          <Stack gap="sm">
+        <form onSubmit={form.onSubmit(() => alert("See you at CMU Marathon"))}>
+          <Stack gap="sm">  
             <Group grow align="start">
               <TextInput
                 label="First Name"
@@ -187,7 +205,7 @@ export default function Home() {
           </Stack>
         </form>
 
-        {/* <Footer year={2023} fullName="Chayanin Suatap" studentId="650610560" /> */}
+        {<Footer year={2024} fullName="Patchanop Rodklueng" studentId="660612151" />}
       </Container>
 
       <TermsAndCondsModal opened={opened} close={close} />
